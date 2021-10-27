9d933e14a8fae1e7d6b1a7d49d3f9cce56b43384
jrbangit <jrbangit03@mercari.com>
2021-10-27 17:04:27 +0900

    Initial commit

---
+++ src/main/java/com/mercari/microservice/dpdatataggingdemo/DpDataTaggingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mercari.microservice.dpdatataggingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpDataTaggingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpDataTaggingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercari/microservice/dpdatataggingdemo/DpDataTaggingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercari.microservice.dpdatataggingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpDataTaggingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
